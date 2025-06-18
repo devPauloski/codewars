bd1f288659d9cdaac2bd512c7c1f15dd974b2679
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-06-18 17:13:56 +0100

    Make changes

---
+++ bouncingBall.js
@@ -0,0 +1,43 @@
+// Bouncing Ball
+
+// How many times will the mother see the ball pass in front of her window 
+// (including when it's falling and bouncing)?
+// Three conditions must be met for a valid experiment:
+
+//     Float parameter "h" in meters must be greater than 0
+//     Float parameter "bounce" must be greater than 0 and less than 1
+//     Float parameter "window" must be less than h.
+
+// If all three conditions above are fulfilled, return a positive integer,
+//  otherwise return -1.
+// Note:
+
+// The ball can only be seen if the height of the rebounding ball
+//  is strictly greater than the window parameter.
+// Examples:
+
+// h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+function bouncingBall(h,  bounce,  window) {
+  // your code here
+  let ballPasses = -1;
+  
+  if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+    ballPasses = 1;
+    
+    while (window < h) {
+      h = h * bounce;
+      if (window < h) {
+        ballPasses = ballPasses + 2;
+      }
+    }
+    
+    return ballPasses;
+  } else {
+    return ballPasses;
+  }
+}
