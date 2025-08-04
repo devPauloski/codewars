1600b0cb83d340462734b509a2fc3281a8a81dca
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-08-04 22:53:38 +0100

    make changes

--- typescript-solutions/src/calculateAverage.ts
+++ typescript-solutions/src/calculateAverage.ts
@@ -2,20 +2,17 @@
 // Write a function which calculates the average of the numbers in a given array.
 
 export function findAverage(array: number[]): number {
-  let result:number;
-  
   if (array.length === 0) {
-    result = 0;
+    return 0;
   } else {
     let sum:number = 0;
     for (let i = 0; i < array.length; i++) {
       sum = sum + array[i];
     }
     
-    result = sum / array.length;
+   return sum / array.length;
   }
-  
-  return result;
 }
 
 console.log(findAverage([1,1, 34]));
+console.log(findAverage([]));
