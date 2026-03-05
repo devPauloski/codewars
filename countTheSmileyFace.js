d1b11d3659a11ddfdbad5cec59f522a793cf2a90
Paul Mgbechi <mpaulchijioke@gmail.com>
2026-03-05 18:41:39 +0100

    update

--- countTheSmileyFace.js
+++ countTheSmileyFace.js
@@ -1,20 +1,54 @@
+/* 
+Count the smiley faces!
+
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+*/
+
 function countSmileys(arr) {
   let count = 0;
-  
+
   for (let i = 0; i < arr.length; i++) {
-    let eyes = arr[i].includes(":") || arr[i].includes(";");
-    let smilingMouth = arr[i].includes(")") || arr[i].includes("D");
-    
-    if (eyes && smilingMouth) {
+    let rules =
+      arr[i].includes(":)") ||
+      arr[i].includes(";)") ||
+      arr[i].includes(":D") ||
+      arr[i].includes(";D") ||
+      arr[i].includes(":-)") ||
+      arr[i].includes(";-)") ||
+      arr[i].includes(":~)") ||
+      arr[i].includes(";~)") ||
+      arr[i].includes(":-D") ||
+      arr[i].includes(";-D") ||
+      arr[i].includes(":~D") ||
+      arr[i].includes(";~D");
+
+    if (rules) {
       count = count + 1;
     }
   }
-  
+
   return count;
 }
 
 console.log(countSmileys([]));
-console.log(countSmileys([':D',':~)',';~D',':)']));
-console.log(countSmileys([':)',':(',':D',':O',':;']));
-console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']));
-console.log(countSmileys([";~)" , ":oD" , ";oD" , ":o>" , ";D" , ":-)" , ";D"]));
+console.log(countSmileys([":D", ":~)", ";~D", ":)"]));
+console.log(countSmileys([":)", ":(", ":D", ":O", ":;"]));
+console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"]));
+console.log(countSmileys([";~)", ":oD", ";oD", ":o>", ";D", ":-)", ";D"]));
