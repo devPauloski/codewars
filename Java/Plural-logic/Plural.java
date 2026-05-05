30eb9d83f4d572bcd61430ae34834a673a6af7ff
Paul Mgbechi <mpaulchijioke@gmail.com>
2026-05-05 05:54:05 +0100

    add

---
+++ Java/Plural-logic/Plural.java
@@ -0,0 +1,15 @@
+public class Plural {
+  public static boolean isPlural(float f) {
+    if ((f >= 0 && f < 1) || (f > 1)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public static void main(String[] args) {
+    System.out.println(isPlural(0.5f));
+    System.out.println(isPlural(1f));
+    System.out.println(isPlural(6f));
+    System.out.println(isPlural(0f));
+  }
+}
